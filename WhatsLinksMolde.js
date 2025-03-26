726229283533c456810b8d709eb236d8faaaabbb
João Pedro Roncalio <joao.p.roncalio@gmail.com>
2025-03-26 18:29:22 -0300

    Update WhatsLinksMolde.js

--- WhatsLinksMolde.js
+++ WhatsLinksMolde.js
@@ -1,23 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elementosWhatsApp = document.querySelectorAll('.box.svelte-lyhjhx'); // Ou use uma classe mais estável se possível
+    // Seleciona todos os elementos que devem ser exibidos ou ocultados
+    var elementosWhatsApp = document.querySelectorAll('.box.svelte-lyhjhx');
+    // Seleciona todos os links que devem acionar a exibição/ocultação
     var links = document.querySelectorAll('.link-whatsapp');
 
-    links.forEach(function (link) {
-        var toggleDisplay = function (event) {
-            event.preventDefault();
+@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
             elementosWhatsApp.forEach(function (elemento) {
                 var displayAtual = window.getComputedStyle(elemento).getPropertyValue('display');
-                // Usando manipulação direta da propriedade
                 if (displayAtual === 'none') {
-                    elemento.style.display = 'flex'; // Ou 'block' se flex não for necessário
+                    elemento.style.setProperty('display', 'flex', 'important');
                 } else {
-                    elemento.style.display = 'none';
+                    elemento.style.setProperty('display', 'none', 'important');
                 }
             });
-        };
-
-        // Adicionando touchstart para melhor resposta em mobile
-        link.addEventListener('touchstart', toggleDisplay);
-        link.addEventListener('click', toggleDisplay);
-    });
-});
+        });
