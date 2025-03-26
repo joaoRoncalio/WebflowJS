b03a0acc50bd856962600abbd040896f8fff01f1
João Pedro Roncalio <joao.p.roncalio@gmail.com>
2025-03-26 18:13:30 -0300

    V2 pra mobile WhatsLinksMolde.js

--- WhatsLinksMolde.js
+++ WhatsLinksMolde.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona todos os elementos que devem ser exibidos ou ocultados
-    var elementosWhatsApp = document.querySelectorAll('.box.svelte-lyhjhx');
-    // Seleciona todos os links que devem acionar a exibição/ocultação
+    var elementosWhatsApp = document.querySelectorAll('.box.svelte-lyhjhx'); // Ou use uma classe mais estável se possível
     var links = document.querySelectorAll('.link-whatsapp');
 
-    // Adiciona um ouvinte de evento a cada link
     links.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // Previne o comportamento padrão do link
-            // Alterna o estado de exibição de cada elemento
+        var toggleDisplay = function (event) {
+            event.preventDefault();
             elementosWhatsApp.forEach(function (elemento) {
                 var displayAtual = window.getComputedStyle(elemento).getPropertyValue('display');
+                // Usando manipulação direta da propriedade
                 if (displayAtual === 'none') {
-                    elemento.style.setProperty('display', 'flex', 'important');
+                    elemento.style.display = 'flex'; // Ou 'block' se flex não for necessário
                 } else {
-                    elemento.style.setProperty('display', 'none', 'important');
+                    elemento.style.display = 'none';
                 }
             });
-        });
+        };
+
+        // Adicionando touchstart para melhor resposta em mobile
+        link.addEventListener('touchstart', toggleDisplay);
+        link.addEventListener('click', toggleDisplay);
     });
 });
