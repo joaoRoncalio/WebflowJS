8e19b1b2ff6e97154ffef8529acf9ad84fce0fdc
João Pedro Roncalio <joao.p.roncalio@gmail.com>
2025-03-26 18:00:07 -0300

    Update WhatsLinksMolde.js

--- WhatsLinksMolde.js
+++ WhatsLinksMolde.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Seleciona todos os elementos que devem ser exibidos ou ocultados
-    var elementosWhatsApp = document.querySelectorAll('.box.svelte-lyhjhx');
+    var elementosWhatsApp = document.querySelectorAll('.box svelte-lyhjhx');
     // Seleciona todos os links que devem acionar a exibição/ocultação
     var links = document.querySelectorAll('.link-whatsapp');
 
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
             elementosWhatsApp.forEach(function (elemento) {
                 var displayAtual = window.getComputedStyle(elemento).getPropertyValue('display');
                 if (displayAtual === 'none') {
-                    elemento.style.setProperty('display', 'flex', 'important');
+                    elemento.style.setProperty('display', 'flex', '!important');
                 } else {
-                    elemento.style.setProperty('display', 'none', 'important');
+                    elemento.style.setProperty('display', 'none', '!important');
                 }
             });
         });
